af8447b561b7cd934f56cc14f0e3ded821123c21
Alex first test <jarb29@gmail.com>
2020-01-28 20:51:44 -0300

    second

---
+++ my-app/src/main.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import mediaObjectList from './mediaObjectList';
+
+
+
+const Main =(props) =>{
+
+    return(
+          <div className="row row-cols-1 row-cols-md-3">
+            <div className="col mb-4">
+              <div className="card h-100">
+                <img src="https://www.visitchile.com/blog/wp-content/uploads/2019/08/monta%C3%B1as-de-chile.jpeg" className="card-img-top" alt="..."/>
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col mb-4">
+              <div className="card h-100">
+                <img src="https://www.visitchile.com/blog/wp-content/uploads/2019/08/monta%C3%B1as-de-chile.jpeg" className="card-img-top" alt="..."/>
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">This is a short card.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col mb-4">
+              <div className="card h-100">
+                <img src="https://www.visitchile.com/blog/wp-content/uploads/2019/08/monta%C3%B1as-de-chile.jpeg" className="card-img-top" alt="..."/>
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col mb-4">
+              <div className="card h-100">
+                <img src="https://www.visitchile.com/blog/wp-content/uploads/2019/08/monta%C3%B1as-de-chile.jpeg" className="card-img-top" alt="..."/>
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+    )
+
+
+}
+
+
+export default Main;
